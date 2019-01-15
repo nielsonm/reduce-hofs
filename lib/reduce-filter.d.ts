209c2d0d6deeb6252e6091f92b91030f8b0a74e8
Michael Nielson <mnielson@redhat.com>
2019-01-14 21:31:13 -0800

    Added filter implementation

---
+++ lib/reduce-filter.d.ts
@@ -0,0 +1,4 @@
+declare const testFilterArray: number[];
+declare const testFilterElement = 17;
+declare const arrayFilter: number[];
+declare const reduceArrayFilter: number[];
---
+++ src/reduce-filter.ts
@@ -0,0 +1,21 @@
+const testFilterArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+const testFilterElement = 17
+
+console.log("Let's find", testFilterElement, " in ", testFilterArray )
+
+const arrayFilter = testFilterArray.filter(filterItem => filterItem == testFilterElement);
+console.log(arrayFilter, "Filtered out")
+
+const reduceArrayFilter = testFilterArray.reduce( function(filtered: number[], cur: number) {
+    if (cur == testFilterElement) {
+        filtered.push(cur)
+    }
+    return filtered
+}, []);
+
+console.log(reduceArrayFilter, "Filtered using reduce")
+
+
+if (arrayFilter.pop() == reduceArrayFilter.pop()) {
+    console.log("Yay, they're equal!")
+}
