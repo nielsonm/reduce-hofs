bf6fa3cf81998253aba718dd699ff8ddc49c9bd3
Michael Nielson <mnielson@redhat.com>
2019-01-21 12:09:43 -0800

    Revert "Remove underscore import from reject.ts"
    This reverts commit 43a836a72de6f9fac81684c4ce5157931c22d932.

--- src/reduce-reject.ts
+++ src/reduce-reject.ts
@@ -1,9 +1,10 @@
+import * as _ from 'underscore';
+
 const testRejectArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
 const testRejectElement = 13
 
 console.log("Let's remove ", testRejectElement, " from ", testRejectArray)
 
-// Use underscore's reject HOF.
 const arrayReject = _.reject(testRejectArray, (num:number) => {
     return num == testRejectElement
 });
